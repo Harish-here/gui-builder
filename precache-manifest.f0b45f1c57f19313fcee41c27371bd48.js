6c27e08728a5dc5d0a0e911c857ee6cd48039aae
Harish Ravi <harish.ravi@ganitinc.com>
2020-06-10 03:21:46 +0530

    Updates

---
+++ precache-manifest.f0b45f1c57f19313fcee41c27371bd48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d59fa942693c305dbd5c2dc882f2286",
+    "url": "/gui-builder/index.html"
+  },
+  {
+    "revision": "0ca2dc4ef9bf7605a536",
+    "url": "/gui-builder/static/css/main.cbb08b63.chunk.css"
+  },
+  {
+    "revision": "438e20f22fbc4360f29e",
+    "url": "/gui-builder/static/js/2.361da772.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gui-builder/static/js/2.361da772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca2dc4ef9bf7605a536",
+    "url": "/gui-builder/static/js/main.23e4a433.chunk.js"
+  },
+  {
+    "revision": "fbe0130e1c7ec3dcc454",
+    "url": "/gui-builder/static/js/runtime-main.47101a2d.js"
+  }
+]);
